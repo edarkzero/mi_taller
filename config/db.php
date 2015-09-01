7dd052d7ef5cebb9265fccc392c46677c16fc05d
Edgar Cardona <edgarcardona87@gmail.com>
2015-09-01 16:50:11 -0430

    billing step 1 #4

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=mi_taller',
+    'dsn' => 'mysql:host=localhost;dbname=mi_taller;port=3307;',
     'username' => 'root',
-    'password' => '',
+    'password' => '123456',
     'charset' => 'utf8',
 ];
